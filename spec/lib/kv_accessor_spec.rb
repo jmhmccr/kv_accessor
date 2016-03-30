babe836b2cd7aa9c90fe4191fa939310db314e06
Jeremiah McCann <jmhmccr@users.noreply.github.com>
2016-03-30 16:07:37 -0700

    Refactored some of the specs for better formatting and easier readability.

--- spec/lib/kv_accessor_spec.rb
+++ spec/lib/kv_accessor_spec.rb
@@ -33,46 +33,49 @@ RSpec.describe KvAccessor do
   # For the expect to change, the duping is somehow necessary
   subject(:data_object) { kv_class.new(default_details.dup, default_other.dup) }
 
-  it 'responds to all the accessor names' do
+  it 'responds to all the accessor names specified' do
     expect(data_object).to respond_to(:make, :year, :blue_interior, :make=,
                                       :year=, :blue_interior=, :model, :price,
                                       :color=, :price=, :upc)
   end
 
-  it 'does not respond to unspecified accessor names' do
+  it 'does not respond to unspecified readers/writers' do
     expect(data_object).not_to respond_to(:submodel, :model=)
   end
 
-  it 'has all of the corresponding reader attributes' do
+  it 'fetches all corresponding reader attributes' do
     expect(data_object).to have_attributes(
       make: 'Chevrolet', model: 'Camaro', blue_interior: 2_000.00,
       price: 40_000.00, year: 1967, upc: '808'
     )
   end
 
-  it 'updates only the explicit keys on attribute assignment' do
-    # There's some funky stuff going on with the ordering between `from()`,
-    # `change { }`, and `expect { }`. `change { }.from` doesn't seem to compare
-    # the objects first, before running the expect statement. I think it only
-    # saves the initial output, runs the expect statement, runs the change block
-    # again, and then finally compares the results. So any order dependent
-    # results need to be another copy.
-    expect {
+  describe 'attribute assignment' do
+    before do
       data_object.make = 'Ford'
       data_object.price = 41_000.00
       data_object.year = 1968
       data_object.blue_interior = 3_000.00
       data_object.color = 'red'
       data_object.upc = '809'
-    }.to change { data_object.details }.from(
-      default_details
-    ).to(
-      default_details.merge(
-        :make => 'Ford', :price => 41_000.00, 'model_year' => 1968,
-        'color' => 'red', { 'leather' => 'blue' } => 3_000.00,
+    end
+
+    it 'updates the specified key-value objects' do
+      expect(data_object).to have_attributes(
+        details: default_details.merge(:make => 'Ford', :price => 41_000.00,
+                                       'model_year' => 1968, 'color' => 'red',
+                                       { 'leather' => 'blue' } => 3_000.00),
+        other: { upc: '809' }
       )
-    ).and change { data_object.other }.from(
-      default_other
-    ).to(upc: '809')
+    end
+
+    it 'all the updates are available via the attribute methods' do
+      expect(data_object).to have_attributes(
+        make: 'Ford', price: 41_000.00, year: 1968, blue_interior: 3_000.00,
+        upc: '809'
+      )
+    end
+  end
+
   end
 end
